fcb3f4814a363103e53b72a286f446f23f24b9f5
Andrej T <andrejtrajchevski@gmail.com>
2014-09-16 00:31:03 +0200

    mostly cosmetic fixes

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ function diff(ours, theirs) {
     var plus = _.difference(ours, common);
     var minus = _.difference(theirs, common);
 
-    console.log(common);
-
     return {
         common: common,
         plus: plus,
@@ -17,16 +15,7 @@ function diff(ours, theirs) {
 var Ingredient = React.createClass({
     displayName: 'Ingredient',
     render: function() {
-        var color;
-        if (this.props.type === 'common') {
-            color = '#999';
-        } else if (this.props.type === 'plus') {
-            color = 'green';
-        } else {
-            color = 'red';
-        }
-
-        return D.li({ style: { color: color }}, this.props.name);
+        return D.li({ className: this.props.type }, this.props.name);
     }
 });
 
@@ -55,18 +44,24 @@ var Ingredients = React.createClass({
 var Thumbnail = React.createClass({
     displayName: 'Thumbnail',
     onClick: function() {
-        this.props.handleChange(this.props.burger);
+        this.props.onSelected(this.props.burger);
     },
     render: function() {
         var burger = this.props.burger;
-        var title = D.div(null, burger.title);
+        var title = D.h2(null, burger.title);
         var image = D.img({ src: burger.imgSrc });
         var ingredients = Ingredients({
             ingredients: burger.ingredients,
             selectedBurger: this.props.selectedBurger
         });
 
-        return D.div({ onClick: this.onClick },
+        var cs = React.addons.classSet;
+        var classes = cs({
+            'thumbnail': true,
+            'selected': this.props.selectedBurger === burger
+        });
+
+        return D.div({ className: classes, onClick: this.onClick },
                      title,
                      image,
                      ingredients);
@@ -98,6 +93,203 @@ var App = React.createClass({
 var burgers = [
     { id: 0, title: 'asd', imgSrc: 'asdf', description: 'qwe', ingredients: ['кечап', 'мајонез'] },
     { id: 1, title: 'asd2', imgSrc: 'asdf', description: '4234', ingredients: ['кечап', 'сенф'] }
+];
+
+var burgers = [
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp1446e004_1a.png",
+        "title": "Classic Burger",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp1446e004_1a.png",
+        "title": "Isabela",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Фриендс премаз",
+            "Марула",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wpfaa533af_1a.png",
+        "title": "Casanova",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Сланина",
+            "Кари сос",
+            "Марула",
+            "Домат",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp1446e004_1a.png",
+        "title": "Mr. Perfect",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Фриендс премаз",
+            "Печурки",
+            "Марула",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wpb8b6b968_1a.png",
+        "title": "Chicago",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Кашкавал",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp1446e004_1a.png",
+        "title": "Firehouse",
+        "ingredients": [
+            "Леб",
+            "Плескавица",
+            "Кашкавал",
+            "Марула",
+            "Чили сос",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wpa509fbfc_1a.png",
+        "title": "Chicken Classic",
+        "ingredients": [
+            "Леб",
+            "Пилешки стек на скара",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp25b62d84_1a.png",
+        "title": "Junior",
+        "ingredients": [
+            "Леб",
+            "Пилешки стек на скара",
+            "Кари сос",
+            "Марула",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp8c227d7c_1a.png",
+        "title": "Senior",
+        "ingredients": [
+            "Леб",
+            "Пилешки стек на скара",
+            "Кашкавал",
+            "Кари сос",
+            "Марула",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp8c227d7c_1a.png",
+        "title": "Hot Chick",
+        "ingredients": [
+            "Леб",
+            "Пилешки стек на скара",
+            "Печурки",
+            "Марула",
+            "Чили сос",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp4c8a7743_1a.png",
+        "title": "Mona Lisa",
+        "ingredients": [
+            "Леб",
+            "Похован пилешки стек",
+            "Фриендс премаз",
+            "Марула",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wpaaabcf82_1a.png",
+        "title": "Daisy",
+        "ingredients": [
+            "Леб",
+            "Похован пилешки стек",
+            "Кари сос",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    },
+    {
+        "imgSrc": "http://burgerfriends.mk/wpimages/wp1c824f2d_1a.png",
+        "title": "Miss Universe",
+        "ingredients": [
+            "Леб",
+            "Ќофте (плескавица) од тиквички",
+            "Френдс премаз",
+            "Марула",
+            "Печурки",
+            "Домат",
+            "Кромид",
+            "Кисели краставички",
+            "Помфрит",
+            "2 соса по избор"
+        ]
+    }
 ];
 
 React.renderComponent(App({ burgers: burgers }), document.getElementById("app"));
